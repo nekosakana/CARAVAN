cfe541ea92736a5ae08d406dcc61397e0e15b5a6
nekosakana <kiri33pudding@gmail.com>
2020-05-19 15:32:13 +0900

    [Start] 4-2

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'blogs/index'
+  get 'blogs/show'
+  get 'blogs/new'
+  get 'blogs/edit'
+  get 'index/show'
+  get 'index/new'
+  get 'index/edit'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
